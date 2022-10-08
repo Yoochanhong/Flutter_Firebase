dc10427ff91179b50636281fc446e14fd230daab
Yoochanhong <chanhongy6@naver.com>
2022-10-08 22:53:15 +0900

    간단한 로그인 화면

--- login_example/lib/main.dart
+++ login_example/lib/main.dart
@@ -1,11 +1,60 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'firebase_options.dart';
+import 'package:login_example/chat_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Authentication(),
+    );
+  }
+}
+
+class Authentication extends StatelessWidget {
+  const Authentication({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+
+      ///로그인, 로그아웃 할때 상태를 추적하는 메소드
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) { ///사용자 인증이 되지 않았을 경우
+          return SignInScreen(
+            providerConfigs: [
+              EmailProviderConfiguration()
+            ],
+          );
+        }
+        return ChatScreen();
+      },
+    );
+  }
 }
