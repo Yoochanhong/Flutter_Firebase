9fc2ffff44c47bab31aa86162b90cfd708931bb9
Yoochanhong <chanhongy6@naver.com>
2022-10-09 17:52:51 +0900

    로그인 창 틀 만들기

--- login_example/lib/main.dart
+++ login_example/lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
+
 import 'firebase_options.dart';
 
 void main() async {
@@ -8,4 +11,49 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Authentication(),
+    );
+  }
+}
+
+class Authentication extends StatelessWidget {
+  const Authentication({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      ///로그인, 로그아웃 상태를 추적
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        ///만약 로그인이 안되어있다면
+        if (!snapshot.hasData) {
+          return SignInScreen(providerConfigs: [
+            ///이메일 인증을 가능하게 해줌
+            EmailProviderConfiguration()
+          ]);
+        }
+        return SuccessScreen();
+      },
+    );
+  }
 }
