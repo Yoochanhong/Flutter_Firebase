be67d1977b9fcd984f0e9a25ff1406d044a00628
Yoochanhong <chanhongy6@naver.com>
2022-10-09 12:33:05 +0900

    로그아웃 버튼

--- login_example/lib/SuccessPage.dart
+++ login_example/lib/SuccessPage.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
 
 class SuccessPage extends StatefulWidget {
   const SuccessPage({Key? key}) : super(key: key);
@@ -16,6 +18,14 @@ class _SuccessPageState extends State<SuccessPage> {
         centerTitle: true,
 
         /// 앱바 제목 가운데 정렬
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut(); ///로그아웃 버튼
+            },
+            icon: Icon(Icons.exit_to_app_sharp),
+          ),
+        ],
       ),
       body: Center(
         child: Text(
