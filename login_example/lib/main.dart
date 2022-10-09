baaa91c9b8155da335eee1324d16bb2b4e013805
Yoochanhong <chanhongy6@naver.com>
2022-10-09 12:35:15 +0900

    간단한 로그인 기능 만들기 완⭐️성

--- login_example/lib/main.dart
+++ login_example/lib/main.dart
@@ -2,8 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
-
 import 'firebase_options.dart';
+import 'SuccessPage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
