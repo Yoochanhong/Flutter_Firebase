e151900723c7eb21a7d3f0575edf23a05a8f5721
Yoochanhong <chanhongy6@naver.com>
2022-10-17 21:51:04 +0900

    crud 뷰 짜기

--- crud_example/lib/main.dart
+++ crud_example/lib/main.dart
@@ -1,11 +1,106 @@
-import 'package:flutter/material.dart';
+import 'dart:core';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
 import 'firebase_options.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var firestore = FirebaseFirestore.instance;
+  var collection = "강좌";
+  var doc = "클래스유";
+  var title;
+
+  @override
+  void initState() {
+    title ??= "";
+    print("init : " + title);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("build : " + title);
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("강의  : $title"),
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Create",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Read",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Update",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Delete",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
