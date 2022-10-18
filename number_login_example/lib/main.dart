5c053f97baddb4b19fc4b5b99ab2f60f8b63b791
Yoochanhong <chanhongy6@naver.com>
2022-10-18 19:38:17 +0900

    streambuilder 구현

--- number_login_example/lib/main.dart
+++ number_login_example/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,4 +10,38 @@ void main() async{
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot){
+        if (!snapshot.hasData){
+          return SignInScreen();
+        }
+        else return HomeScreen();
+      },
+    );
+  }
 }
