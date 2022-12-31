1fb43646cd52714aa4369b3e5bf1a243ebdcf77d
맥북 유찬홍 <chanhongy6@naver.com>
2023-01-01 01:43:08 +0900

    뭔가 됐으니까 커밋

--- github_login_example/lib/login.dart
+++ github_login_example/lib/login.dart
@@ -1,43 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:github_login/github_login.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:github_sign_in/github_sign_in.dart';
 import 'package:github_login_example/private.dart';
+import 'package:github_sign_in/github_sign_in.dart';
+import 'package:github_login/github_login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Future<void> githubLogin() async {
-      final GitHubSignIn gitHubSignIn = GitHubSignIn(
-          clientId: clientId,
-          clientSecret: clientSecret,
-          redirectUrl: redirectUrl);
-      var result = await gitHubSignIn.signIn(context);
-      switch (result.status) {
-        case GitHubSignInResultStatus.ok:
-          print(result.token);
-          break;
+    Future<UserCredential> signInWithGitHub() async {
+      // Create a new provider
+      GithubAuthProvider githubProvider = GithubAuthProvider();
 
-        case GitHubSignInResultStatus.cancelled:
-        case GitHubSignInResultStatus.failed:
-          print(result.errorMessage);
-          break;
-      }
+      return await FirebaseAuth.instance.signInWithProvider(githubProvider);
     }
 
     return Scaffold(
       body: Center(
-        child: MaterialButton(
-          color: Colors.black,
-          onPressed: () {
-            githubLogin();
-          },
-          child: Text(
-            '깃허브 로그인',
-            style: TextStyle(fontSize: 30, color: Colors.white),
-          ),
+        child: ElevatedButton(
+            onPressed: () {
+              signInWithGitHub();
+            },
+            child: Text('깃허브 로그인')),
+      ),
+    );
+  }
+}
+
+class Success extends StatelessWidget {
+  const Success({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Success"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: const [Text("로그인에 성공하셨습니다!")],
         ),
       ),
     );
--- github_login_example/lib/main.dart
+++ github_login_example/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:github_login_example/login.dart';
+import 'package:github_login_example/private.dart';
+import 'dart:convert';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,52 +21,74 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyPage(),
+      home: LoginPage(),
     );
   }
 }
 
-class MyPage extends StatefulWidget {
-  const MyPage({Key? key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
-  State<MyPage> createState() => _MyPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _MyPageState extends State<MyPage> {
+class _LoginPageState extends State<LoginPage> {
+  final flutterWebviewPlugin = FlutterWebviewPlugin();
+  String? accessToken;
+
+  @override
+  void initState() {
+    super.initState();
+    flutterWebviewPlugin.onUrlChanged.listen((url) {
+      if (url.startsWith(redirectUrl)) {
+        final code = url.split('code=')[1];
+        getAccessToken(code);
+      }
+    });
+  }
+
+  void getAccessToken(String code) async {
+    final response = await http.post(
+      Uri.parse('https://github.com/login/oauth/access_token'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: jsonEncode({
+        'client_id': clientId,
+        'client_secret': clientSecret,
+        'code': code,
+      }),
+    );
+    final responseJson = jsonDecode(response.body);
+    setState(() {
+      accessToken = responseJson['access_token'];
+    });
+    print(accessToken);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              icon: Icon(Icons.chevron_left)),
-        ),
-        body: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return Login();
-            } else {
-              return Container(
-                width: 300,
-                height: 300,
-                child: Center(
-                  child: Text(
-                    '${snapshot.data!.displayName}님 반갑습니다.',
-                    style: TextStyle(
-                      fontSize: 24,
-                    ),
-                  ),
-                ),
-              );
-            }
-          },
-        ),
-      ),
+    return Scaffold(
+      body: accessToken == null
+          ? Center(
+              child: ElevatedButton(
+                child: const Text('Login with GitHub'),
+                onPressed: () {
+                  flutterWebviewPlugin.launch(
+                    'https://github.com/login/oauth/authorize?client_id=$clientId&redirect_uri=$redirectUrl',
+                    clearCache: true,
+                    clearCookies: true,
+                  );
+                },
+              ),
+            )
+          : const Center(
+              child: Text(
+                '성공',
+              ),
+            ),
     );
   }
 }
